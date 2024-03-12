699626c7ad44d2db9305505effae29da77624513
Nombre-S <78140736+Nombre-S@users.noreply.github.com>
2024-03-12 21:46:46 +0000

    Implemented cookies per session

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import re
 import redis
+from http.cookies import SimpleCookie
+import uuid
 
 mappings = {(r"^/books/(?P<book_id>\d+)$", "get_books"),
             (r"^/books/(?P<book_id>\d+)$", "get_books"),
@@ -9,6 +11,25 @@ mappings = {(r"^/books/(?P<book_id>\d+)$", "get_books"),
 r = redis.StrictRedis(host="localhost", port=6379, db=0)
 
 class WebRequestHandler(BaseHTTPRequestHandler):
+   
+    def cookies(self):
+        return SimpleCookie(self.headers.get("Cookie"))
+
+    def get_session(self):
+        cookies = self.cookies()
+        session_id = None
+        if not cookies:
+            session_id = uuid.uuid4()
+        else:
+            session_id = cookies["session_id"].value
+        return session_id
+            
+    def write_session_cookie(self, session_id):
+        cookies = SimpleCookie()
+        cookies["session_id"] = session_id
+        cookies["session_id"]["max-age"] = 1000
+        self.send_header("Set-Cookie", cookies.output(header=""))
+
     def do_GET(self):
         self.url_mapping_response()
 
@@ -32,13 +53,20 @@ class WebRequestHandler(BaseHTTPRequestHandler):
             return match.groupdict()
 
     def get_books(self, book_id):
+        session_id = self.get_session()
+        r.lpush(f"session: {session_id}", f"book: {book_id}")
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
+        self.write_session_cookie(session_id)
         self.end_headers()
-        # book_info = f"<h1> Info de libro {book_id} es correcto </h1>".encode("utf-8")
-        book_info = r.get(f"book: {book_id}") or "No existe el libro".encode("utf-8")
-        self.wfile.write(book_info)
- 
+        book_info = r.get(f"book: {book_id}") or "<h1> No existe el libro </h1>".encode("utf-8")
+        self.wfile.write(str(book_info).encode("utf-8"))
+        self.wfile.write(f"session: {session_id}".encode("utf-8"))
+        book_list = r.lrange(f"session: {session_id}", 0, 1)
+        for book in book_list:
+            self.wfile.write(f"book: {book}".encode("utf-8"))
+    
+   
     def index(self):
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
